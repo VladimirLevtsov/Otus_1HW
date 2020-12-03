38b3e8f683fe045013aca30a2fae83a873291cf9
Vladimir Levtsov <levtsov.vladimir@gmail.com>
2020-12-03 23:36:10 +0300

    Initial Commit

---
+++ FirstLessonHomeTask/FirstLessonHomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstLessonHomeTaskApp.swift
+//  FirstLessonHomeTask
+//
+//  Created by VLADIMIR LEVTSOV on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FirstLessonHomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
